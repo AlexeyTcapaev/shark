496fedc547de43c9e080dede0979b3a8233bb266
Цапаев Алексей <gonzolexx@gmail.com>
2018-12-12 03:17:10 +0300

    ym fix

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -7,25 +7,25 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>Бизнес-Акула</title>
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <!-- Yandex.Metrika counter -->
-    <script src="https://mc.yandex.ru/metrika/watch.js" type="text/javascript"></script>
-    <script type="text/javascript" >
-    try {
-        var yaCounter33686679 = new Ya.Metrika({
-            id:33686679,
-            clickmap:true,
-            trackLinks:true,
-            accurateTrackBounce:true,
-            webvisor:true,
-            trackHash:true
-        });
-    } catch(e) { }
-    </script>
-    <!-- /Yandex.Metrika counter -->
 </head>
 <body>
   <div id="app">
   </div>
   <script src="{{ mix('js/app.js') }}"></script>
+      <!-- Yandex.Metrika counter -->
+      <script src="https://mc.yandex.ru/metrika/watch.js" type="text/javascript"></script>
+      <script type="text/javascript" >
+      try {
+          var yaCounter33686679 = new Ya.Metrika({
+              id:33686679,
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true,
+              trackHash:true
+          });
+      } catch(e) { }
+      </script>
+      <!-- /Yandex.Metrika counter -->
 </body>
 </html>
